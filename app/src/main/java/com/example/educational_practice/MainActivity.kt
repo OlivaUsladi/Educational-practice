fa5dfefbc5e898fd53c723510bc219b7ba069e12
OlivaUsladi <alya.maysner.04@mail.ru>
2024-12-01 00:02:47 +0300

    Создание страницы авторизации

--- app/src/main/java/com/example/educational_practice/MainActivity.kt
+++ app/src/main/java/com/example/educational_practice/MainActivity.kt
@@ -4,13 +4,32 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.educational_practice.ui.theme.EducationalpracticeTheme
 import kotlinx.coroutines.selects.RegistrationFunction
 
@@ -19,7 +38,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            AuthorizationScreen()
         }
     }
 }
@@ -31,5 +50,48 @@ fun RegistrationScreen(){
 
 @Composable
 fun AuthorizationScreen(){
-
+    var login = remember { mutableStateOf("") }
+    var password = remember { mutableStateOf("") }
+    Column (modifier = Modifier.fillMaxSize().background(Color(0xFFFFFEFA))) {
+        Image(painter = painterResource(R.drawable.img),
+            contentDescription = "logo",
+            modifier = Modifier
+                .padding(top=134.dp)
+                .width(144.dp)
+                .height(169.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+        Box(Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)) {
+            Text(
+                text = "The Road to adulthood",
+                color = Color(0xFFA47676),
+                modifier = Modifier.align(Alignment.Center),
+                fontSize = 32.sp
+            )
+        }
+        Box(Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)) {
+            TextField(modifier = Modifier.padding(top = 150.dp).align(Alignment.Center).height(40.dp),
+                value = login.value,
+                onValueChange = { login.value = it },
+                label = {
+                    Text(
+                        text = "Enter login/email",
+                        fontSize = 24.sp,
+                        textAlign = TextAlign.Center
+                    )
+                })
+        }
+        Box(Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)) {
+            TextField(modifier = Modifier.padding(top=50.dp).align(Alignment.Center).height(40.dp),
+                value = password.value,
+                onValueChange = {password.value = it},
+                label = { Text(text = "Enter password", fontSize = 24.sp, textAlign = TextAlign.Center) })
+        }
+        Box(Modifier.fillMaxWidth().align(Alignment.CenterHorizontally)){
+            Text(text = "Registration",
+                color = Color(0xFFA47676),
+                modifier = Modifier.align(Alignment.Center).padding(top=40.dp),
+                fontSize = 24.sp)
+        }
+    }
 }
